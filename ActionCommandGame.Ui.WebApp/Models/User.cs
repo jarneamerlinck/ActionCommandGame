998a69553eb2cf6493dfae8de834dba333bf7ede
jarneamerlinck <39766278+jarneamerlinck@users.noreply.github.com>
2022-04-01 11:16:59 +0200

    update

---
+++ ActionCommandGame.Ui.WebApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ActionCommandGame.Ui.WebApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("User Name")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
--- ActionCommandGame.Ui.WebApp/Program.cs
+++ ActionCommandGame.Ui.WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,6 +24,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Authenticate}/{action=Index}/{id?}");
 
 app.Run();
