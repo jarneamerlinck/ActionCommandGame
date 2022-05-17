043bc08cbedefa138dc45d79388eed978e52b0fc
jarneamerlinck <39766278+jarneamerlinck@users.noreply.github.com>
2022-05-17 11:21:01 +0200

    FOR error fixing

--- ActionCommandGame.Ui.WebApp/Models/PlayerAction.cs
+++ ActionCommandGame.Ui.WebApp/Models/PlayerAction.cs
@@ -6,7 +6,9 @@ namespace ActionCommandGame.Ui.WebApp.Models
     public class PlayerAction
     {
         public PlayerResult Player { get; set; }
+        public int Id { get; set; }
         public GameResult? GameResult { get; set; }
         public IList<ServiceMessage>? Messages { get; set; }
+        public IList<PlayerItemResult>? Items { get; set; }
     }
 }
