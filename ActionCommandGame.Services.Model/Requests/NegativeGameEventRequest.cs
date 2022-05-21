812821f79e8c99143d6361706d5d5f1ec03a375c
jarneamerlinck <39766278+jarneamerlinck@users.noreply.github.com>
2022-05-21 10:12:25 +0200

    Added required on atributes

--- ActionCommandGame.Services.Model/Requests/NegativeGameEventRequest.cs
+++ ActionCommandGame.Services.Model/Requests/NegativeGameEventRequest.cs
@@ -1,15 +1,22 @@
-﻿using ActionCommandGame.Abstractions;
+﻿using System.ComponentModel.DataAnnotations;
+using ActionCommandGame.Abstractions;
 
 namespace ActionCommandGame.Services.Model.Requests
 {
     public class NegativeGameEventRequest: IHasProbability
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string DefenseWithGearDescription { get; set; }
+        [Required]
         public string DefenseWithoutGearDescription { get; set; }
+        [Required]
         public int DefenseLoss { get; set; }
+        [Required]
         public int Probability { get; set; }
     }
 }
--- ActionCommandGame.Services.Model/Requests/PositiveGameEventRequest.cs
+++ ActionCommandGame.Services.Model/Requests/PositiveGameEventRequest.cs
@@ -1,14 +1,20 @@
-﻿using ActionCommandGame.Abstractions;
+﻿using System.ComponentModel.DataAnnotations;
+using ActionCommandGame.Abstractions;
 
 namespace ActionCommandGame.Services.Model.Requests
 {
     public class PositiveGameEventRequest: IHasProbability
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public int Money { get; set; }
+        [Required]
         public int Experience { get; set; }
+        [Required]
         public int Probability { get; set; }
     }
 }
